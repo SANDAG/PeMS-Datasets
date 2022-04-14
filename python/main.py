a472eb05e3f0435ccb1c5872be4e3276bf5e9da1
JYSD1 <49728623+JYSD1@users.noreply.github.com>
2022-04-14 15:22:00 -0700

    add bulk insert holidays table component

--- python/main.py
+++ python/main.py
@@ -18,6 +18,7 @@ mapping = [
     {"file": "d11_text_station_aadt", "tbl": "pems.station_aadt"},
     {"file": "d11_text_station_day", "tbl": "pems.station_day"},
     {"file": "d11_text_station_hour", "tbl": "pems.station_hour"},
+    {"file": "pems_holiday_insert", "tbl": "pems.holiday"},
 ]
 
 # for each PeMS data-set load all files in the data folder
@@ -28,37 +29,42 @@ for root, dirs, files in os.walk("../data/"):
         print("Loading: " + file)
         path = os.path.join(root, file)
 
-        # only expect ZIP or GZ files in the data folder
-        if not (file.endswith(".gz") or file.endswith(".zip")):
-            msg = "unexpected file extension (.gz or .zip files expected)"
+        # only expect ZIP or GZ or TXT files in the data folder
+        if not (file.endswith(".gz") or file.endswith(".zip") or
+                file.endswith(".txt")):
+            msg = "unexpected file extension (.gz or .zip ot .txt files expected)"
             raise ValueError(msg)
         else:
-            # if the compressed archive is a ZIP file extract the compressed
-            # GZ file from the ZIP archive, this is required only for the
-            # PeMS Data Clearinghouse Station AADT source of which we only
-            # extract the Station AADT Month Hour data-set
-            if file.endswith(".zip"):
-                with ZipFile(path) as zipfile:  # extract file of interest
-                    for info in zipfile.infolist():
-                        if "d11_text_station_aadt_month_hours_" in info.filename:
-                            zipfile.extract(info.filename, path=root)
-                            file = info.filename
-                os.remove(path)
-                path = os.path.join(root, file)
-
-            # write out the underlying data file from the compressed GZ file
-            writePath = path.rstrip(".gz")
-            with open(writePath, "wb") as writeFile:
-                # the Census V-Class Hour data-sets have 112 empty fields appended
-                # past the fields provided in the metadata
-                if "all_text_tmg_vclass_hour" in file:
-                    for line in gzip.open(path):
-                        line = line.replace(b","*112, b"")
-                        writeFile.write(line)
-                else:
-                    writeFile.write(gzip.open(path).read())
-            writeFile.close()
-
+            if file.endswith(".txt"):
+                writePath = path
+            
+            else:
+                # if the compressed archive is a ZIP file extract the compressed
+                # GZ file from the ZIP archive, this is required only for the
+                # PeMS Data Clearinghouse Station AADT source of which we only
+                # extract the Station AADT Month Hour data-set
+                if file.endswith(".zip"):
+                    with ZipFile(path) as zipfile:  # extract file of interest
+                        for info in zipfile.infolist():
+                            if "d11_text_station_aadt_month_hours_" in info.filename:
+                                zipfile.extract(info.filename, path=root)
+                                file = info.filename
+                    os.remove(path)
+                    path = os.path.join(root, file)
+                
+                # write out the underlying data file from the compressed GZ file
+                writePath = path.rstrip(".gz")
+                with open(writePath, "wb") as writeFile:
+                    # the Census V-Class Hour data-sets have 112 empty fields appended
+                    # past the fields provided in the metadata
+                    if "all_text_tmg_vclass_hour" in file:
+                        for line in gzip.open(path):
+                            line = line.replace(b","*112, b"")
+                            writeFile.write(line)
+                    else:
+                        writeFile.write(gzip.open(path).read())
+                writeFile.close()
+                
         # bulk load underlying data file to SQL Server
         # destination is dependent on the PeMS Data Clearinghouse source
         sqlTbl = None
@@ -69,7 +75,7 @@ for root, dirs, files in os.walk("../data/"):
         if sqlTbl is None:
             msg = "no SQL destination mapped"
             raise ValueError(msg)
-        else:
+        else:            
             sql = "BULK INSERT " + sqlTbl + " FROM '" + os.path.realpath(writePath) + "' " + \
                   "WITH (TABLOCK, CODEPAGE = 'ACP', FIELDTERMINATOR=',', ROWTERMINATOR='0x0a');"
             cursor.execute(sql)
