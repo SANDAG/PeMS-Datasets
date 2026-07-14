3e45a280c9b4e360f1094515fdb1dce5b92b8b53
Timothy Andersen <tan@sandag.org>
2026-07-14 10:51:51 -0700

    Move year and data set mode selection to toml config file

--- python/extract_parquet.py
+++ python/extract_parquet.py
@@ -3,6 +3,7 @@
 from pathlib import Path
 
 import polars as pl
+import toml
 from tqdm import tqdm
 
 STATION_5MIN_READ_SCHEMA = pl.Schema(
@@ -112,6 +113,11 @@ STATION_DAY_READ_SCHEMA = pl.Schema(
 )
 
 
+CONFIG = toml.load("./config.toml")
+PEMS_YEARS: list[int] = CONFIG["pems_years"]  # pyright: ignore[reportAny]
+PEMS_MODES: list[str] = CONFIG["pems_modes"]  # pyright: ignore[reportAny]
+
+
 def extract_station_5min(txt_path: Path) -> pl.DataFrame:
     station_5minute = pl.read_csv(
         txt_path,
@@ -141,44 +147,47 @@ def extract_station_day(txt_path: Path) -> pl.DataFrame:
 
 if __name__ == "__main__":
     # TODO: Base hardcoded loop on some kind of config/environment variable
-    for year in [2025, 2026]:
+    for year in PEMS_YEARS:
         for month in range(1, 13):
+            if "station_5min" in PEMS_MODES:
+                txt_paths = [
+                    path
+                    for path in Path(
+                        f"./data/pems/txt/station_5min/{year}/{month:02}/"
+                    ).iterdir()
+                    if path.suffix == ".txt"
+                ]
+                for txt_path in tqdm(txt_paths):
+                    df = extract_station_5min(txt_path)
+                    parquet_name = txt_path.stem + ".parquet"
+                    dir_ = Path(
+                        f"./data/pems/parquet/station_5min/{year}/{df['timestamp'].dt.month().first():02}/"
+                    )
+                    dir_.mkdir(parents=True, exist_ok=True)
+                    df.write_parquet(dir_ / f"{parquet_name}")
+
+        if "station_hour" in PEMS_MODES:
             txt_paths = [
                 path
-                for path in Path(
-                    f"./data/pems/txt/station_5min/{year}/{month:02}/"
-                ).iterdir()
+                for path in Path(f"./data/pems/txt/station_hour/{year}").iterdir()
                 if path.suffix == ".txt"
             ]
             for txt_path in tqdm(txt_paths):
-                df = extract_station_5min(txt_path)
+                df = extract_station_hour(txt_path)
                 parquet_name = txt_path.stem + ".parquet"
-                dir_ = Path(
-                    f"./data/pems/parquet/station_5min/{year}/{df['timestamp'].dt.month().first():02}/"
-                )
+                dir_ = Path(f"./data/pems/parquet/station_hour/{year}/")
                 dir_.mkdir(parents=True, exist_ok=True)
                 df.write_parquet(dir_ / f"{parquet_name}")
 
-        txt_paths = [
-            path
-            for path in Path(f"./data/pems/txt/station_hour/{year}").iterdir()
-            if path.suffix == ".txt"
-        ]
-        for txt_path in tqdm(txt_paths):
-            df = extract_station_hour(txt_path)
-            parquet_name = txt_path.stem + ".parquet"
-            dir_ = Path(f"./data/pems/parquet/station_hour/{year}/")
-            dir_.mkdir(parents=True, exist_ok=True)
-            df.write_parquet(dir_ / f"{parquet_name}")
-
-        txt_paths = [
-            path
-            for path in Path(f"./data/pems/txt/station_day/{year}").iterdir()
-            if path.suffix == ".txt"
-        ]
-        for txt_path in tqdm(txt_paths):
-            df = extract_station_day(txt_path)
-            parquet_name = txt_path.stem + ".parquet"
-            dir_ = Path(f"./data/pems/parquet/station_day/{year}/")
-            dir_.mkdir(parents=True, exist_ok=True)
-            df.write_parquet(dir_ / f"{parquet_name}")
+        if "station_day" in PEMS_MODES:
+            txt_paths = [
+                path
+                for path in Path(f"./data/pems/txt/station_day/{year}").iterdir()
+                if path.suffix == ".txt"
+            ]
+            for txt_path in tqdm(txt_paths):
+                df = extract_station_day(txt_path)
+                parquet_name = txt_path.stem + ".parquet"
+                dir_ = Path(f"./data/pems/parquet/station_day/{year}/")
+                dir_.mkdir(parents=True, exist_ok=True)
+                df.write_parquet(dir_ / f"{parquet_name}")
